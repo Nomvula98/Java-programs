c4eededc43b1e9b05a4328934807161b10939638
Nomvula <nomvula.skosana@xgileit.co.za>
2021-05-19 09:42:15 +0200

    Encapsulation

--- src/com/classes/BankAccount.java
+++ src/com/classes/BankAccount.java
@@ -1,2 +1,26 @@
-package com.classes;public class BankAccount {
+package com.classes;
+
+//Encapsulated class
+public class BankAccount {
+    //Encapsulation: Process of crating a class by binding variables and methods
+        //as one unit and then operating variables only through
+        //these methods
+    //1. Declare all variables as private
+    //2. Create a pair of getter and setter methods
+
+    private double balance;
+
+    public void setBalance(double balance){
+        if (balance <=0) {
+            System.out.println("Do not enter a negative amount");
+        }else{
+            this.balance=balance;
+        }
+    }
+    public double getBalance(){
+        return balance;
+    }
+
+
+
 }
--- src/com/classes/Clerk.java
+++ src/com/classes/Clerk.java
@@ -1,2 +1,11 @@
-package com.classes;public class Clerk {
+package com.classes;
+
+public class Clerk {
+
+    public static void main(String[] args){
+        BankAccount b1=new BankAccount();
+        b1.setBalance(200);
+        System.out.println(b1.getBalance());
+    }
+
 }
